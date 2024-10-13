7e8915be297b3549e23f7cf64003527d2e0aaebb
ananda17gb <ananda.widigdo17@gmail.com>
2024-10-13 21:08:15 +0700

    fixing no image on vercel

--- src/components/pages/techstack/techStackData.js
+++ src/components/pages/techstack/techStackData.js
@@ -2,61 +2,61 @@ export const techStackData = [
   {
     techStackType: "Website",
     techStackName: "CSS3",
-    techStackImage: "/public/css3.png",
+    techStackImage: "public/css3.png",
   },
   {
     techStackType: "Website",
     techStackName: "TailwindCSS",
-    techStackImage: "/public/tailwindcss.png",
+    techStackImage: "public/tailwindcss.png",
   },
   {
     techStackType: "Website",
     techStackName: "HTML5",
-    techStackImage: "/public/html5.png",
+    techStackImage: "public/html5.png",
   },
   {
     techStackType: "Website",
     techStackName: "JavaScript",
-    techStackImage: "/public/javascript.png",
+    techStackImage: "public/javascript.png",
   },
   {
     techStackType: "Website",
     techStackName: "React",
-    techStackImage: "/public/react.png",
+    techStackImage: "public/react.png",
   },
   {
     techStackType: "Mobile",
     techStackName: "Flutter",
-    techStackImage: "/public/flutter.png",
+    techStackImage: "public/flutter.png",
   },
   {
     techStackType: "Other",
     techStackName: "Figma",
-    techStackImage: "/public/figma.png",
+    techStackImage: "public/figma.png",
   },
   {
     techStackType: "Other",
     techStackName: "Firebase",
-    techStackImage: "/public/firebase.png",
+    techStackImage: "public/firebase.png",
   },
   {
     techStackType: "Other",
     techStackName: "MySQL",
-    techStackImage: "/public/mysqllogo.png",
+    techStackImage: "public/mysqllogo.png",
   },
   {
     techStackType: "Other",
     techStackName: "Golang",
-    techStackImage: "/public/golang.png",
+    techStackImage: "public/golang.png",
   },
   {
     techStackType: "Other",
     techStackName: "Python",
-    techStackImage: "/public/python.png",
+    techStackImage: "public/python.png",
   },
   {
     techStackType: "Other",
     techStackName: "C++",
-    techStackImage: "/public/c++.png",
+    techStackImage: "public/c++.png",
   },
 ];
